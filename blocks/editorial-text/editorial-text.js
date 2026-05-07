69b1830f3f3a78170b22c3eef1a740ec79e07e50
Jgrosskurth <159270531+Jgrosskurth@users.noreply.github.com>
2026-05-07 02:25:32 +0000

    rebuild files

--- blocks/editorial-text/editorial-text.js
+++ blocks/editorial-text/editorial-text.js
@@ -102,7 +102,7 @@ export default function decorate(block) {
           }
         });
       },
-      { threshold: 0.15 },
+      { threshold: 0.05, rootMargin: '300px 0px 300px 0px' },
     );
 
     io.observe(inner);
